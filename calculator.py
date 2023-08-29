02fa01ca1c65f88f9386fe6a9cc2e40d13bd84ea
Oshada Samarasinghe <oshadasa@gmail.com>
2023-08-29 16:34:22 +0530

    Adding more advanced functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def addition(x, y):
     return x + y
 
@@ -12,12 +14,32 @@ def division(x, y):
         return x / y
     else:
         return "Can't divide by zero"
+    
+def square_root(x):
+    return math.sqrt(x)
+
+def exponentiation(x, y):
+    return x ** y
+
+def sin(x):
+    return math.sin(math.radians(x))
+
+def cos(x):
+    return math.cos(math.radians(x))
+
+def tan(x):
+    return math.tan(math.radians(x))
 
 operations = {
     '+': addition,
     '-': subtraction,
     '*': multiplication,
-    '/': division
+    '/': division,
+    'sr': square_root,
+    '**': exponentiation,
+    'sin': sin,
+    'cos': cos,
+    'tan': tan
 }
 
 history = []
@@ -28,16 +50,21 @@ while True:
     print("2. Subtraction (-)")
     print("3. Multiplication (*)")
     print("4. Division (/)")
-    print("5. Show History")
-    print("6. Close")
+    print("5. Square Root (sr)")
+    print("6. Exponentiation (**)")
+    print("7. Sin (sin)")
+    print("8. Cos (cos)")
+    print("9. Tan (tan)")
+    print("10. Show History")
+    print("11. Close")
 
-    choice = input("Enter choice (+/-/*///5/6): ")
+    choice = input("Enter choice (+/-/*///sr/**/sin/cos/tan/10/11): ")
 
-    if choice == '6':
+    if choice == '11':
         print("Bye, See you next time!")
         break
 
-    if choice == '5':
+    if choice == '10':
         print("Calculation History:")
         for history_list in history:
             print(history_list)
@@ -45,12 +72,16 @@ while True:
         continue
 
     if choice in operations:
-        operator = operations[choice]
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
+        if choice in ('sr','sin','cos','tan'):
+            num = float(input("Enter a number: "))
+            result = operations [choice] (num)
+            calculation = f"{choice}{num} = {result}"
+        else:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            result = operations[choice](num1, num2)
+            calculation = f"{num1} {choice} {num2} = {result}"
         
-        result = operator(num1, num2)
-        calculation = f"{num1} {choice} {num2} = {result}"
         history.append(calculation)
         print("Result:", calculation)
         print(" ")
